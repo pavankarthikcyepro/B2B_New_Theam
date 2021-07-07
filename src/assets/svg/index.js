b9492b8f223db84e356dc638be601b32cf31f5e3
Nirosha <niroshab@auto-mate.info>
2021-07-07 10:44:53 +0530

    Iamges are added

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -1,8 +1,312 @@
+const HOME_FILL = require("../images/home_fill.svg");
+const HOME_LINE = require("../images/home_line.svg");
+const EMS_LINE = require("../images/ems_line.svg");
+const SCHEDULE_FILL = require("../images/schedule_fill.svg");
+const SCHEDULE_LINE = require("../images/my_schedule.svg");
+const ADD = require("../images/add.svg");
+const AUTO = require("../images/auto.svg");
+const ADMINISTRATION = require("../images/administration.svg");
+const ALLOTMENT = require("../images/allotment.svg");
+const ADJUST = require("../images/adjust.svg");
+const AUTOMATE_LOGO = require("../images/automate_logo.svg");
+const BOOKING_PAYMENT_MODE = require("../images/booking_payment_mode.svg");
+const BOOKING_TRACKER = require("../images/booking_tracker.svg");
+const BOOK = require("../images/book.svg");
+const BOOKINGS = require("../images/bookings.svg");
+const CALL_FOLLOW_UP = require("../images/call_follow_up");
+const CAR = require("../images/car.svg");
+const CONTACT = require("../images/contact.svg");
+const CHATBOT = require("../images/chatbot.svg");
+const CHECKED = require("../images/checked.svg");
+const CHECKBOX_INACTIVE = require("../images/checkbox_inactive.svg");
+const CHECKBOX_SELECTED = require("../images/checkbox_selected.svg");
+const CHECKBOX_MARKED_CIRCLE_OUTLINE = require("../images/checkbox_marked_circle_outline.svg");
+const CHEVRON = require("../images/chevron.svg");
+const CHEVRON_COPY = require("../images/chevron_copy.svg");
+const CLIP_OUTLINE = require("../images/clip_outline.svg");
+const CLOSE = require("../images/close.svg");
+const COST = require("../images/cost.svg");
+const COMMITMENT = require("../images/commitment.svg");
+const COMMUNICATION_DETAILS = require("../images/communication_details.svg");
+const COMMUNOCATION = require("../images/communocation.svg");
+const CREATE_NEW = require("../images/create_new.svg");
+const CREATE_NEW_FAB = require("../images/create_new_fab.svg");
+const CUSTOMER_NEED_ANALYSIS = require("../images/customer_need_analysis.svg");
+const CUSTOMER_PROFILE = require("../images/customer_profile.svg");
+const CUSTOMER_RELATIONSHIP = require("../images/customer_relationship.svg");
+const DATE_RANGE = require("../images/date_range.svg");
+const DELIVERY = require("../images/delivery.svg");
+const DIGITAL_PAYMENT = require("../images/digital_payment.svg");
+const DOT = require("../images/dot.svg");
+const DOCUMENT_FOR_VERIFICATION = require("../images/document_for_verification.svg");
+const DOCUMENT_UPLOAD = require("../images/document_upload.svg");
+const DOCUMENT_WALLET = require("../images/document_wallet.svg");
+const DOCUMENTS = require("../images/documents.svg");
+const DOWNARROW = require("../images/downarrow.svg");
+const DOWNLOAD = require("../images/download.svg");
+const DRAFT = require("../images/draft.svg");
+const EARNED_INCENTIVE = require("../images/earned_incentive.svg");
+const EDIT = require("../images/edit.svg");
+const END = require("../images/end.svg");
+const EMAIL_FOLLOW_UP = require("../images/email_follow_up.svg");
+const EVENT_MANAGEMENT = require("../images/event_management.svg");
+const EXCHANGE_DETAILS = require("../images/exchange_details.svg");
+const EXCEL_DOWNLOAD = require("../images/excel_download.svg");
+const FILTER = require("../images/filter.svg");
+const FORKLIFT = require("../images/forklift.svg");
+const FINANCE_DETAILS = require("../images/finance_details.svg");
+const FINANCE_DO_LETTER = require("../images/finance_do_letter.svg");
+const FINANCE = require("../images/finance.svg");
+const ENQUIRY = require("../images/enquiry.svg");
+const GROUP2567 = require("../images/group2567.svg");
+const GROUP2568 = require("../images/group2568.svg");
+const GROUP2569 = require("../images/group2569.svg");
+const GROUP2570 = require("../images/group2570.svg");
+const GROUP2571 = require("../images/group2571.svg");
+const HOME_RUN = require("../images/home_run.svg");
+const INAUGURATION = require("../images/inauguration.svg");
+const INDICATOR = require("../images/indicator.svg");
+const INVOICE = require("../images/invoice.svg");
+const KNOWLEDGE_FAB = require("../images/knowledge_fab.svg");
+const KEYPAD = require("../images/keypad.svg");
+const LOGOUT = require("../images/logout.svg");
+const LEFTARROW = require("../images/leftarrow.svg");
+const RIGHTARROW = require("../images/rightarrow.svg");
+const MANAGE = require("../images/manage.svg");
+const MAIL = require("../images/mail.svg");
+const MONEY = require("../images/money.svg");
+const MENU = require("../images/menu.svg");
+const MODEL_SELECTIONCOPY = require("../images/model_selectioncopy.svg");
+const MODEL_SELECTION = require("../images/model_selection.svg");
+const MONTH = require("../images/month.svg");
+const MORE = require("../images/more.svg");
+const MUTE = require("../images/mute.svg");
+const MATE = require("../images/mate.svg");
+const MYSCHEDULE = require("../images/myschedule.svg");
+const MY_ATTENDENCE = require("../images/my_attendence.svg");
+const MY_PERFORMANCE = require("../images/my_performance.svg");
+const NEXT = require("../images/next.svg");
+const NEW_CAR_PENCIL_PRINT = require("../images/new_car_pencil_print.svg");
+const NEW_ENQUIRES = require("../images/new_enquires.svg");
+const NOTEPAD = require("../images/notepad.svg");
+const NOTIFICATION = require("../images/notification.svg");
+const NOTIFY = require("../images/notify.svg");
+const OFFER_FILL = require("../images/offer_fill.svg");
+const OFFER_LINE = require("../images/offer_line.svg");
+const OPEN_AND_PENDING_ENQUIRES = require("../images/open_and_pending_enquires.svg");
+const PATH6 = require("../images/path6.svg");
+const PATH5 = require("../images/path5.svg");
+const PATH63 = require("../images/path63.svg");
+const PAUSE = require("../images/pause.svg");
+const PATH1383 = require("../images/path1383.svg");
+const PATH1705 = require("../images/path1705.svg");
+const PATH2198 = require("../images/path2198.svg");
+const PATH2195 = require("../images/path2195.svg");
+const PAYMENT_RECIEPTS = require("../images/payment_reciepts.svg");
+const PDF_DOWNLOAD = require("../images/pdf_download.svg");
+const PDF = require("../images/pdf.svg");
+const PEN = require("../images/pen.svg");
+const PHONE = require("../images/phone.svg");
+const PENDING = require("../images/pending.svg");
+const PERSONAL_DETAILSCOPY = require("../images/personal_detailscopy.svg");
+const PERSONAL_DETAILS = require("../images/personal_details.svg");
+const PERCENT = require("../images/percent.svg");
+const PLUS = require("../images/plus.svg");
+const PLACEHOLDER = require("../images/placeholder.svg");
+const POTENTIAL_INCENTIVE = require("../images/potential_incentive.svg");
+const PRICE_CONFIRMATION = require("../images/price_confirmation.svg");
+const PRINT = require("../images/price.svg");
+const PURSE = require("../images/purse.svg");
+const QR_CODE = require("../images/qr_code.svg");
+const REGISTER = require("../images/register.svg");
+const REPORT_FILL = require("../images/report_fill.svg");
+const REPORT_LINE = require("../images/report_line.svg");
+const ROLEMANAGEMENT = require("../images/rolemanagements.svg");
+const SALE = require("../images/sale.svg");
+const SAVE = require("../images/save.svg");
+const SCORE_CARD = require("../images/score_card.svg");
+const SEARCH = require("../images/search.svg");
+const SERVICES = require("../images/services.svg");
+const SETTINGS = require("../images/settings.svg");
+const SIM_CARD = require("../images/sim_card.svg");
+const SPEED = require("../images/speed.svg");
+const STOCK_CARD_1 = require("../images/stock_card_1.svg");
+const STOCK_CARD_2 = require("../images/stock_card_2.svg");
+const STOCK_FILL = require("../images/stock_fill.svg");
+const STOCK_LINE = require("../images/stock_fill.svg");
+const SUPPORT = require("../images/support.svg");
+const SURFACE1 = require("../images/surface1.svg");
+const SEO_REPORT = require("../images/seo_report.svg");
+const SHOPPING_BAG = require("../images/shopping_bag.svg");
+const TERRITORY_ALLOCATION = require("../images/territory_allocation.svg");
+const TODAY = require("../images/today.svg");
+const TODAY_ENQUIRES = require("../images/today_enquires.svg");
+const TICK = require("../images/tick.svg");
+const TESTING = require("../images/testing.svg");
+const UNDERTAKING_LETTERS = require("../images/undertaking_letters.svg");
+const UPARROW = require("../images/uparrow.svg");
+const UPLOAD = require("../images/upload.svg");
+const VEHICLE_EXCHANGE = require("../images/vehicle_exchange.svg");
+const VEHICLE_ALLOTMENT_DETAILS = require("../images/vehicle_allotment_details.svg");
+const VEHICLES = require("../images/vehicles.svg");
+const WEEK = require("../images/week.svg");
+const YEAR = require("../images/year.svg");
 
-const HOME_FILL = require('../images/home_fill.svg');
-const HOME_LINE = require('../images/home_line.svg');
-const EMS_LINE = require('../images/ems_line.svg');
-const SCHEDULE_FILL = require('../images/schedule_fill.svg');
-const SCHEDULE_LINE = require('../images/my_schedule.svg');
 
-export { HOME_FILL, HOME_LINE, EMS_LINE, SCHEDULE_FILL, SCHEDULE_LINE };
+
+export {
+  HOME_FILL,
+  HOME_LINE,
+  EMS_LINE,
+  SCHEDULE_FILL,
+  SCHEDULE_LINE,
+  ADD,
+  AUTO,
+  ADMINISTRATION,
+  ADJUST,
+  ALLOTMENT,
+  AUTOMATE_LOGO,
+  BOOK,
+  BOOKING_PAYMENT_MODE,
+  BOOKING_TRACKER,
+  BOOKINGS,
+  CALL_FOLLOW_UP,
+  CAR,
+  CONTACT,
+  CHATBOT,
+  CHECKED,
+  CHECKBOX_INACTIVE,
+  CHECKBOX_SELECTED,
+  CHECKBOX_MARKED_CIRCLE_OUTLINE,
+  CHEVRON,
+  CHEVRON_COPY,
+  CLIP_OUTLINE,
+  CLOSE,
+  COST,
+  CHECKBOX_SELECTED,
+  COMMITMENT,
+  COMMUNICATION_DETAILS,
+  COMMUNOCATION,
+  CREATE_NEW,
+  CREATE_NEW_FAB,
+  CUSTOMER_NEED_ANALYSIS,
+  CUSTOMER_PROFILE,
+  CUSTOMER_RELATIONSHIP,
+  DATE_RANGE,
+  DELIVERY,
+  DIGITAL_PAYMENT,
+  DOT,
+  DOCUMENT_FOR_VERIFICATION,
+  DOCUMENT_UPLOAD,
+  DOCUMENT_WALLET,
+  DOCUMENTS,
+  DOWNARROW,
+  DOWNLOAD,
+  DRAFT,
+  EARNED_INCENTIVE,
+  EDIT,
+  END,
+  EMAIL_FOLLOW_UP,
+  EVENT_MANAGEMENT,
+  EXCHANGE_DETAILS,
+  EXCEL_DOWNLOAD,
+  FILTER,
+  FORKLIFT,
+  KEYPAD,
+  FINANCE_DETAILS,
+  FINANCE_DO_LETTER,
+  FINANCE,
+  ENQUIRY,
+  GROUP2567,
+  GROUP2568,
+  GROUP2569,
+  GROUP2570,
+  GROUP2571,
+  HOME_RUN,
+  INAUGURATION,
+  INDICATOR,
+  INVOICE,
+  KNOWLEDGE_FAB,
+  LOGOUT,
+  LEFTARROW,
+  MANAGE,
+  MENU,
+  MATE,
+  MAIL,
+  MUTE,
+  MONEY,
+  MODEL_SELECTIONCOPY,
+  MODEL_SELECTION,
+  MONTH,
+  MORE,
+  MYSCHEDULE,
+  MY_ATTENDENCE,
+  MY_PERFORMANCE,
+  NEXT,
+  NEW_CAR_PENCIL_PRINT,
+  NEW_ENQUIRES,
+  NOTEPAD,
+  NOTIFICATION,
+  NOTIFY,
+  OFFER_FILL,
+  OFFER_LINE,
+  OPEN_AND_PENDING_ENQUIRES,
+  PATH5,
+  PATH6,
+  PATH63,
+  PAUSE,
+  PATH1383,
+  PATH1705,
+  PATH2195,
+  PATH2198,
+  PAYMENT_RECIEPTS,
+  PDF_DOWNLOAD,
+  PEN,
+  PDF,
+  PENDING,
+  PERSONAL_DETAILSCOPY,
+  PERSONAL_DETAILS,
+  PERCENT,
+  PLUS,
+  PHONE,
+  PLACEHOLDER,
+  POTENTIAL_INCENTIVE,
+  PRICE_CONFIRMATION,
+  PRINT,
+  PURSE,
+  QR_CODE,
+  REGISTER,
+  REPORT_FILL,
+  REPORT_LINE,
+  RIGHTARROW,
+  ROLEMANAGEMENT,
+  SCORE_CARD,
+  SEARCH,
+  SERVICES,
+  SETTINGS,
+  SPEED,
+  SALE,
+  SAVE,
+  SUPPORT,
+  SIM_CARD,
+  STOCK_CARD_1,
+  STOCK_CARD_2,
+  STOCK_FILL,
+  STOCK_LINE,
+  SURFACE1,
+  SEO_REPORT,
+  SHOPPING_BAG,
+  TERRITORY_ALLOCATION,
+  TODAY,
+  TODAY_ENQUIRES,
+  TICK,
+  TESTING,
+  UNDERTAKING_LETTERS,
+  UPARROW,
+  UPLOAD,
+  VEHICLE_EXCHANGE,
+  VEHICLE_ALLOTMENT_DETAILS,
+  VEHICLES,
+  WEEK,
+  YEAR,
+};
