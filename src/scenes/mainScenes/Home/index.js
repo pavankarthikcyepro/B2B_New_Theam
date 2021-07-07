c35cd1d435ce4fb47f6a8a32c430c9da77335fdb
Ravinder <ravindrak@bharatgroupe.com>
2021-07-07 18:16:14 +0530

    working on home page ui

--- src/scenes/mainScenes/Home/index.js
+++ src/scenes/mainScenes/Home/index.js
@@ -1,17 +1,60 @@
 
 import React from 'react';
-import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet, FlatList, Dimensions, Image } from 'react-native';
 import { Colors } from '../../../styles';
-import { HOME_FILL, HOME_LINE } from '../../../assets/svg'; // import SVG
+import { Searchbar } from 'react-native-paper';
+import { IconButton } from 'react-native-paper';
+import VectorImage from 'react-native-vector-image';
+import { FILTER } from '../../../assets/svg'
+
+const screenWidth = Dimensions.get('window').width;
+const itemWidth = (screenWidth - 30) / 2;
+const data = [1, 2]
 
 const HomeScreen = ({ navigation }) => {
 
+    const [searchQuery, setSearchQuery] = React.useState('');
+
+
+    const onChangeSearch = query => setSearchQuery(query);
+
     return (
         <SafeAreaView style={styles.container}>
+            <View style={{ flex: 1, paddingVertical: 10, paddingHorizontal: 15 }}>
+                <View style={{ flexDirection: 'row', height: 60, alignItems: 'center', justifyContent: 'space-between' }}>
+                    <Searchbar
+                        style={{ width: '90%' }}
+                        placeholder="Search"
+                        onChangeText={onChangeSearch}
+                        value={searchQuery}
+                    />
+                    <VectorImage
+                        width={25}
+                        height={25}
+                        source={FILTER}
+                        style={{ tintColor: Colors.DARK_GRAY }}
+                    />
+                </View>
 
-            <Text>{'HOME'}</Text>
-            {/* <HOME_FILL width={20} height={20} color={'green'} /> */}
-
+                <FlatList
+                    data={data}
+                    numColumns={2}
+                    horizontal={false}
+                    keyExtractor={(item, index) => index.toString()}
+                    renderItem={({ item, index }) => {
+                        return (
+                            <View style={{ width: itemWidth, height: 200, padding: 5 }}>
+                                <View style={styles.shadow}>
+                                    <Image
+                                        style={{ width: '100%', height: 100 }}
+                                        source={require('../../../assets/images/bently.png')}
+                                    />
+                                </View>
+                            </View>
+                        )
+                    }}
+                />
+            </View>
         </SafeAreaView>
     )
 }
@@ -24,7 +67,20 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'column',
         justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: Colors.WHITE
+        backgroundColor: Colors.LIGHT_GRAY
     },
+    shadow: {
+        overflow: 'hidden',
+        borderRadius: 4,
+        width: '90%',
+        height: '100%',
+        shadowColor: Colors.DARK_GRAY,
+        shadowOffset: {
+            width: 0,
+            height: 3
+        },
+        shadowRadius: 5,
+        shadowOpacity: 1.0,
+        backgroundColor: 'red'
+    }
 })
