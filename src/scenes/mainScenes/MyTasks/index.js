7bc21aa318fa13f5a43b4c96943cf552a95d5a4f
Ravinder <ravindrak@bharatgroupe.com>
2021-07-09 17:20:27 +0530

    issues fixed

--- src/scenes/mainScenes/MyTasks/index.js
+++ src/scenes/mainScenes/MyTasks/index.js
@@ -7,100 +7,43 @@ import {
   FlatList,
   Dimensions,
 } from "react-native";
-import { Colors } from "../../../styles";
+import { Colors, GlobalStyle } from "../../../styles";
+import { MyTaskItem } from '../../../pureComponents/myTaskItem';
+import { useDispatch, useSelector } from 'react-redux';
 
 const screenWidth = Dimensions.get("window").width;
 
-const datalist = [
-  {
-    TaskName: "Proceed to Booking",
-    TaskStatus: "ASSIGNED",
-    CreatedOn: "Tue Jun 29 2021",
-    DMSLead: "GSGHS Vshsj",
-    PhoneNo: "Gsghs 8488464949",
-  },
-  {
-    TaskName: "Test Drive",
-    TaskStatus: "SENT_FOR_APPROVAL",
-    CreatedOn: "Tue Jun 29 2021",
-    DMSLead: "GSGHS Vshsj",
-    PhoneNo: "Gsghs 8488464949",
-  },
-  {
-    TaskName: "Pre Booking Follow Up",
-    TaskStatus: "ASSIGNED",
-    CreatedOn: "Tue Jun 29 2021",
-    DMSLead: "GSGHS Vshsj",
-    PhoneNo: "Gsghs 8488464949",
-  },
-  {
-    TaskName: "Home Visit",
-    TaskStatus: "ASSIGNED",
-    CreatedOn: "Tue Jun 29 2021",
-    DMSLead: "GSGHS Vshsj",
-    PhoneNo: "Gsghs 8488464949",
-  },
-  {
-    TaskName: "Enguiry Follow Up",
-    TaskStatus: "ASSIGNED",
-    CreatedOn: "Tue Jun 29 2021",
-    DMSLead: "GSGHS Vshsj",
-    PhoneNo: "Gsghs 8488464949",
-  },
-];
-
 const MyTasksScreen = ({ navigation }) => {
+
+  const selector = useSelector(state => state.mytaskReducer);
+
   return (
     <SafeAreaView style={styles.container}>
-      <FlatList
-        data={datalist}
-        keyExtractor={(item, index) => index.toString()}
-        ItemSeparatorComponent={() => {
-          return (
-            <View
-              style={{
-                width: screenWidth - 40,
-                height: 20,
-                backgroundColor: Colors.LIGHT_GRAY,
-              }}
-            ></View>
-          );
-        }}
-        renderItem={({ item, index }) => {
-          return (
-            <View style={styles.list}>
-              <View style={{ flexDirection: "row" }}>
-                <Text style={{ color: Colors.GRAY, fontSize: 16 }}>
-                  TaskName:
-                </Text>
-                <Text style={{ fontSize: 16 }}>{item.TaskName}</Text>
+      <View style={styles.view1}>
+        <FlatList
+          data={selector.tableData}
+          keyExtractor={(item, index) => index.toString()}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => {
+            return (
+              <View style={styles.separator}></View>
+            );
+          }}
+          renderItem={({ item, index }) => {
+            return (
+              <View style={[styles.listBgVw]}>
+                <MyTaskItem
+                  taskName={item.taskName}
+                  status={item.taskStatus}
+                  created={item.createdOn}
+                  dmsLead={item.dmsLead}
+                  phone={item.phoneNo}
+                />
               </View>
-              <View style={{ flexDirection: "row" }}>
-                <Text style={{ color: Colors.GRAY, fontSize: 16 }}>
-                  Task Status:
-                </Text>
-                <Text style={{ fontSize: 16 }}>{item.TaskStatus}</Text>
-              </View>
-              <View style={{ flexDirection: "row" }}>
-                <Text style={{ color: Colors.GRAY, fontSize: 16 }}>
-                  Created On:
-                </Text>
-                <Text style={{ fontSize: 16 }}>{item.CreatedOn}</Text>
-              </View>
-              <View style={{ flexDirection: "row", fontSize: 16 }}>
-                <Text style={{ color: Colors.GRAY }}>DMS Lead: </Text>
-                <Text style={{ fontSize: 16 }}>{item.DMSLead}</Text>
-              </View>
-              <View style={{ flexDirection: "row" }}>
-                <Text style={{ color: Colors.GRAY, fontSize: 16 }}>
-                  Phone No:
-                </Text>
-                <Text style={{ fontSize: 16 }}>{item.PhoneNo}</Text>
-              </View>
-            </View>
-          );
-        }}
-      />
+            );
+          }}
+        />
+      </View>
     </SafeAreaView>
   );
 };
@@ -110,18 +53,19 @@ export default MyTasksScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    alignItems: "flex-start",
     backgroundColor: Colors.LIGHT_GRAY,
-    padding: 20,
   },
-  list: {
-    padding: 20,
-    width: screenWidth - 20,
-    height: screenWidth - 250,
+  view1: {
+    paddingHorizontal: 20,
+    marginTop: 10
+  },
+  listBgVw: {
+    width: screenWidth - 40,
     backgroundColor: Colors.WHITE,
-    borderRadius: 10,
-    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    paddingVertical: 5
   },
+  separator: {
+    height: 10,
+  }
 });
