cd597cd858f288164661d99725935587452a1064
Nirosha <niroshab@auto-mate.info>
2021-07-12 18:43:47 +0530

    setting functionality added

--- src/scenes/mainScenes/settingsScreen.js
+++ src/scenes/mainScenes/settingsScreen.js
@@ -1,22 +1,125 @@
-import React from 'react';
-import { SafeAreaView, View, Text, StyleSheet } from 'react-native';
+// import React from "react";
+// import {
+//   SafeAreaView,
+//   View,
+//   Text,
+//   StyleSheet,
+//   FlatList,
+//   Dimensions,
+// } from "react-native";
+// import { Colors } from "../../styles";
+// import { settingsScreenItem } from "../..pureComponents/settingsScreenItem";
+// const screenWidth = Dimensions.get("window").width;
+
+// const datalist = [
+//   {
+//     name: "My Profile",
+//   },
+//   {
+//     name: "Change Password",
+//   },
+// ];
+// const SettingsScreen = () => {
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <FlatList
+//         data={datalist}
+//         keyExtractor={(item, index) => index.toString()}
+//         ItemSeparatorComponent={() => {
+//           return (
+//             <View
+//               style={{
+//                 width: screenWidth - 20,
+//                 height: 20,
+//                 backgroundColor: Colors.LIGHT_PINK,
+//               }}
+//             ></View>
+//           );
+//         }}
+//         renderItem={({ item, index }) => {
+//           return <Text>{item.name}</Text>;
+//         }}
+//       />
+//     </SafeAreaView>
+//   );
+// };
+
+// export default SettingsScreen;
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flexDirection: "column",
+//     justifyContent: "flex-start",
+//     alignItems: "flex-start",
+//   },
+// });
+import React from "react";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Dimensions,
+} from "react-native";
+import { Colors } from "../../styles";
+import {} from "../../pureComponents/settingScreenItem";
+const screenWidth = Dimensions.get("window").width;
+
+const datalist = [
+  {
+    name: "My Profile",
+  },
+  {
+    name: "Change Password",
+  },
+];
 
 const SettingsScreen = () => {
-
-    return (
-        <SafeAreaView style={styles.container}>
-            <Text>{'Notification'}</Text>
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={datalist}
+        keyExtractor={(item, index) => index.toString()}
+        ItemSeparatorComponent={() => {
+          return (
+            <View
+              style={{
+                width: screenWidth,
+                height: 10,
+                backgroundColor: Colors.LIGHT_GRAY,
+              }}
+            ></View>
+          );
+        }}
+        renderItem={({ item, index }) => {
+          return (
+            <View style={styles.list}>
+              <View style={{ flexDirection: "row" }}>
+                <Text style={{ fontSize: 14, fontWeight: "bold" }}>
+                  {item.name}
+                </Text>
+              </View>
+            </View>
+          );
+        }}
+      />
+    </SafeAreaView>
+  );
+};
 
 export default SettingsScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: Colors.LIGHT_GRAY,
+    paddingTop: 10,
+  },
+  list: {
+    padding: 20,
+    backgroundColor: Colors.WHITE,
+    borderRadius: 10,
+  },
+});
