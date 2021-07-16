ab29ecef612b71c736471a4e812055aa31e048fc
mysayamunadevi <yamunadevimysa@gmail.com>
2021-07-16 13:31:13 +0530

    done addPreEnquiryScreen

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 # B2B_New_Theam
 
 To display the svg icons run below command before npx react-native run-android
+yarn react-native-vector-image generate
+->
 
- -> yarn react-native-vector-image generate
+To display the vector icons used:
 
-To display the vector icons used: 
+-> react-native-vector-image
+-> @klarna/react-native-vector-drawable
 
- -> react-native-vector-image 
- -> @klarna/react-native-vector-drawable
- 
-To display top tab navigation, used: 
+To display top tab navigation, used:
 
- -> @react-navigation/material-top-tabs 
- -> react-native-tab-view@^2.16.0
- 
+-> @react-navigation/material-top-tabs
+-> react-native-tab-view@^2.16.0
--- src/scenes/mainScenes/EMS/addPreEnquiryScreen.js
+++ src/scenes/mainScenes/EMS/addPreEnquiryScreen.js
@@ -1,200 +1,269 @@
-import React, { useState } from 'react';
-import { SafeAreaView, ScrollView, View, Text, StyleSheet, Dimensions, Pressable } from 'react-native';
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Pressable,
+} from "react-native";
 import { ButtonComp } from "../../../components/buttonComp";
-import { Checkbox, Button, IconButton } from 'react-native-paper';
-import { Colors, GlobalStyle } from '../../../styles';
-import { TextinputComp } from '../../../components/textinputComp';
-import { DropDownComponant } from '../../../components/dropDownComp';
+import { Checkbox, Button, IconButton } from "react-native-paper";
+import { Colors, GlobalStyle } from "../../../styles";
+import { TextinputComp } from "../../../components/textinputComp";
+import { DropDownComponant } from "../../../components/dropDownComp";
 
-const screenWidth = Dimensions.get('window').width;
-
+const screenWidth = Dimensions.get("window").width;
 
 const DropDownSelectionComponant = ({ label, value, onPress }) => {
-    return (
-        <Pressable onPress={onPress}>
-            <View style={{ height: 70, backgroundColor: Colors.WHITE, justifyContent: 'flex-end' }}>
-                <View style={styles.view3}>
-                    <Text style={styles.text3}>{label}</Text>
-                    <IconButton
-                        icon="menu-down"
-                        color={Colors.BLACK}
-                        size={30}
-                        onPress={() => { }}
-                    />
-                </View>
-                <Text style={{ width: '100%', height: 0.5, backgroundColor: Colors.GRAY }}></Text>
-            </View>
-        </Pressable>
-    )
-}
+  return (
+    <Pressable onPress={onPress}>
+      <View
+        style={{
+          height: 70,
+          backgroundColor: Colors.WHITE,
+          justifyContent: "flex-end",
+        }}
+      >
+        <View style={styles.view3}>
+          <Text style={styles.text3}>{label}</Text>
+          <IconButton
+            icon="menu-down"
+            color={Colors.BLACK}
+            size={30}
+            onPress={() => {}}
+          />
+        </View>
+        <Text
+          style={{ width: "100%", height: 0.5, backgroundColor: Colors.GRAY }}
+        ></Text>
+      </View>
+    </Pressable>
+  );
+};
 
 const AddPreEnquiryScreen = () => {
+  const [checked, setChecked] = useState(false);
+  const [text, setText] = useState("");
+  const [dropdownVisible, setDropDownVisible] = useState(false);
+  const [multiDropdownVisible, setMultiDropDownVisible] = useState(false);
 
-    const [checked, setChecked] = useState(false);
-    const [text, setText] = useState("");
-    const [dropdownVisible, setDropDownVisible] = useState(false);
-    const [multiDropdownVisible, setMultiDropDownVisible] = useState(false)
+  return (
+    <SafeAreaView style={styles.container}>
+      <DropDownComponant
+        visible={dropdownVisible}
+        multiple={false}
+        selectedItems={(items) => {
+          console.log("selected: ", items);
+          setDropDownVisible(false);
+        }}
+      />
 
+      <DropDownComponant
+        visible={multiDropdownVisible}
+        multiple={true}
+        selectedItems={(items) => {
+          console.log("selected: ", items);
+          setMultiDropDownVisible(false);
+        }}
+      />
 
-
-    return (
-        <SafeAreaView style={styles.container}>
-
-            <DropDownComponant
-                visible={dropdownVisible}
-                multiple={false}
-                selectedItems={(items) => {
-                    console.log('selected: ', items);
-                    setDropDownVisible(false);
-                }}
+      <ScrollView
+        automaticallyAdjustContentInsets={true}
+        bounces={true}
+        contentContainerStyle={{ padding: 10 }}
+        style={{ flex: 1 }}
+      >
+        <Text style={styles.text1}>{"Create New Pre-Enquiry"}</Text>
+        <View style={styles.view1}>
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Checkbox.Android
+              status={checked ? "checked" : "unchecked"}
+              uncheckedColor={Colors.DARK_GRAY}
+              color={Colors.RED}
+              onPress={() => {
+                setChecked(!checked);
+              }}
             />
-
-            <DropDownComponant
-                visible={multiDropdownVisible}
-                multiple={true}
-                selectedItems={(items) => {
-                    console.log('selected: ', items);
-                    setMultiDropDownVisible(false);
-                }}
-            />
-
-            <ScrollView
-                automaticallyAdjustContentInsets={true}
-                bounces={true}
-                contentContainerStyle={{ padding: 10 }}
-                style={{ flex: 1 }}
+            <Text style={styles.text2}>{"Create enquiry"}</Text>
+          </View>
+          <Button
+            labelStyle={{
+              fontSize: 12,
+              fontWeight: "400",
+              color: Colors.BLUE,
+              textTransform: "none",
+            }}
+            onPress={() => {}}
+          >
+            Reset
+          </Button>
+        </View>
+        <View>
+          <View style={[styles.addView, GlobalStyle.shadow]}>
+            <View
+              style={{ width: 320, margin: 5, height: 30, borderRadius: 8 }}
             >
-                <Text style={styles.text1}>{'Create New Pre-Enquiry'}</Text>
-                <View style={styles.view1}>
-                    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-                        <Checkbox.Android
-                            status={checked ? 'checked' : 'unchecked'}
-                            uncheckedColor={Colors.DARK_GRAY}
-                            color={Colors.RED}
-                            onPress={() => {
-                                setChecked(!checked);
-                            }}
-                        />
-                        <Text style={styles.text2}>{'Create enquiry'}</Text>
-                    </View>
-                    <Button
-                        labelStyle={{ fontSize: 12, fontWeight: '400', color: Colors.BLUE, textTransform: 'none' }}
-                        onPress={() => { }}
-                    >
-                        Reset
-                    </Button>
-                </View>
+              <TextinputComp
+                style={{ height: 70 }}
+                value={text}
+                label={"First Name"}
+                onChangeText={(text) => setText(text)}
+              />
+              <Text
+                style={{
+                  width: "100%",
+                  height: 0.5,
+                  backgroundColor: Colors.GRAY,
+                }}
+              ></Text>
+              <TextinputComp
+                style={{ height: 70 }}
+                value={text}
+                label={"Last Name"}
+                onChangeText={(text) => setText(text)}
+              />
+              <Text
+                style={{
+                  width: "100%",
+                  height: 0.5,
+                  backgroundColor: Colors.GRAY,
+                }}
+              ></Text>
+              <TextinputComp
+                style={{ height: 70 }}
+                value={text}
+                label={"Mobile Number"}
+                keyboardType={"numeric"}
+                onChangeText={(text) => setText(text)}
+              />
+              <Text
+                style={{
+                  width: "100%",
+                  height: 0.5,
+                  backgroundColor: Colors.GRAY,
+                }}
+              ></Text>
+              <TextinputComp
+                style={{ height: 70 }}
+                value={text}
+                label={"Alternate Mobile Number"}
+                keyboardType={"numeric"}
+                onChangeText={(text) => setText(text)}
+              />
+              <Text
+                style={{
+                  width: "100%",
+                  height: 0.5,
+                  backgroundColor: Colors.GRAY,
+                }}
+              ></Text>
+              <TextinputComp
+                style={{ height: 70 }}
+                value={text}
+                label={"Email-Id"}
+                keyboardType={"email-address"}
+                onChangeText={(text) => setText(text)}
+              />
+              <Text
+                style={{
+                  width: "100%",
+                  height: 0.5,
+                  backgroundColor: Colors.GRAY,
+                }}
+              ></Text>
 
-                <View style={[{ borderRadius: 6, }]}>
-                    <TextinputComp
-                        style={{ height: 70 }}
-                        value={text}
-                        label={'First Name'}
-                        onChangeText={(text) => setText(text)}
-                    />
-                    <Text style={{ width: '100%', height: 0.5, backgroundColor: Colors.GRAY }}></Text>
-                    <TextinputComp
-                        style={{ height: 70 }}
-                        value={text}
-                        label={'Last Name'}
-                        onChangeText={(text) => setText(text)}
-                    />
-                    <Text style={{ width: '100%', height: 0.5, backgroundColor: Colors.GRAY }}></Text>
-                    <TextinputComp
-                        style={{ height: 70 }}
-                        value={text}
-                        label={'Mobile Number'}
-                        onChangeText={(text) => setText(text)}
-                    />
-                    <Text style={{ width: '100%', height: 0.5, backgroundColor: Colors.GRAY }}></Text>
-                    <TextinputComp
-                        style={{ height: 70 }}
-                        value={text}
-                        label={'Alternate Mobile Number'}
-                        onChangeText={(text) => setText(text)}
-                    />
-                    <Text style={{ width: '100%', height: 0.5, backgroundColor: Colors.GRAY }}></Text>
-                    <TextinputComp
-                        style={{ height: 70 }}
-                        value={text}
-                        label={'Email-Id'}
-                        onChangeText={(text) => setText(text)}
-                    />
-                    <Text style={{ width: '100%', height: 0.5, backgroundColor: Colors.GRAY }}></Text>
+              <DropDownSelectionComponant
+                label={"Select Model"}
+                onPress={() => setDropDownVisible(true)}
+              />
+              <DropDownSelectionComponant
+                label={"Select Enquiry Segment"}
+                onPress={() => setMultiDropDownVisible(true)}
+              />
+              <DropDownSelectionComponant
+                label={"Select Customer Type"}
+                onPress={() => setDropDownVisible(true)}
+              />
+              <DropDownSelectionComponant
+                label={"Select Source of Pre-Enquiry"}
+                onPress={() => setMultiDropDownVisible(true)}
+              />
 
-                    <DropDownSelectionComponant
-                        label={'Select Model'}
-                        onPress={() => setDropDownVisible(true)}
-                    />
-                    <DropDownSelectionComponant
-                        label={'Select Enquiry Segment'}
-                        onPress={() => setMultiDropDownVisible(true)}
-                    />
-                    <DropDownSelectionComponant
-                        label={'Select Customer Type'}
-                        onPress={() => setDropDownVisible(true)}
-                    />
-                    <DropDownSelectionComponant
-                        label={'Select Source of Pre-Enquiry'}
-                        onPress={() => setMultiDropDownVisible(true)}
-                    />
-
-                    <TextinputComp
-                        style={{ height: 70 }}
-                        value={text}
-                        label={'Pincode'}
-                        onChangeText={(text) => setText(text)}
-                    />
-                    <Text style={{ width: '100%', height: 0.5, backgroundColor: Colors.GRAY }}></Text>
-                </View>
-
-                <View style={styles.view2}>
-                    <ButtonComp
-                        title={"SUBMIT"}
-                        width={screenWidth - 40}
-                        onPress={() => { }}
-                    />
-                </View>
-            </ScrollView>
-        </SafeAreaView >
-    )
-}
+              <TextinputComp
+                style={{ height: 70 }}
+                value={text}
+                label={"Pincode"}
+                keyboardType={"numeric"}
+                onChangeText={(text) => setText(text)}
+              />
+              <Text
+                style={{
+                  width: "100%",
+                  height: 0.5,
+                  backgroundColor: Colors.GRAY,
+                }}
+              ></Text>
+            </View>
+          </View>
+          <View style={styles.view2}>
+            <ButtonComp
+              title={"SUBMIT"}
+              width={screenWidth - 60}
+              onPress={() => {}}
+            />
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default AddPreEnquiryScreen;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        padding: 10
-    },
-    text1: {
-        fontSize: 16,
-        fontWeight: '700',
-        paddingLeft: 5
-    },
-    view1: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        height: 60,
-    },
-    view2: {
-        marginTop: 10,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    view3: {
-        width: '100%', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', backgroundColor: Colors.WHITE
-    },
-    text2: {
-        fontSize: 14,
-        fontWeight: '600'
-    },
-    text3: {
-        paddingLeft: 15,
-        fontSize: 16,
-        fontWeight: '400',
-        color: Colors.GRAY
-    }
-})
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    padding: 10,
+  },
+  text1: {
+    fontSize: 16,
+    fontWeight: "700",
+    paddingLeft: 5,
+  },
+  view1: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    height: 60,
+  },
+  view2: {
+    marginTop: 690,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  view3: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: Colors.WHITE,
+  },
+  text2: {
+    fontSize: 14,
+    fontWeight: "600",
+  },
+  text3: {
+    paddingLeft: 15,
+    fontSize: 16,
+    fontWeight: "400",
+    color: Colors.GRAY,
+  },
+
+  addView: {
+    color: Colors.WHITE,
+    position: "relative",
+  },
+});
