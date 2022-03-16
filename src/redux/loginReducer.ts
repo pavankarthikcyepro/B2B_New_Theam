ece91001e6543afda2c43fc1d102712174ebdb5b
hemachandra <hemachandrak855@gamil.com>
2022-03-16 17:11:06 +0530

    working on issues

--- src/redux/loginReducer.ts
+++ src/redux/loginReducer.ts
@@ -57,244 +57,242 @@ const initialState: LoginState = {
   branchesList: []
 }
 
-export const postUserData = createAsyncThunk('LOGIN_SLICE/postUserData', async (inputData, { rejectWithValue }) => {
-
-  export const postUserData = createAsyncThunk(
-    "LOGIN_SLICE/postUserData",
-    async (inputData, { rejectWithValue }) => {
-      const response = await client.post(URL.LOGIN(), inputData);
-      const json = await response.json();
-      if (!response.ok) {
-        return rejectWithValue(json);
-      }
-      return json;
+export const postUserData = createAsyncThunk(
+  "LOGIN_SLICE/postUserData",
+  async (inputData, { rejectWithValue }) => {
+    const response = await client.post(URL.LOGIN(), inputData);
+    const json = await response.json();
+    if (!response.ok) {
+      return rejectWithValue(json);
     }
-  );
+    return json;
+  }
+);
 
-  export const getMenuList = createAsyncThunk(
-    "LOGIN_SLICE/getMenuList",
-    async (name, { rejectWithValue }) => {
-      const response = await client.get(URL.MENULIST_API(name));
-      const json = await response.json();
-      if (!response.ok) {
-        return rejectWithValue(json);
-      }
-      return json;
+export const getMenuList = createAsyncThunk(
+  "LOGIN_SLICE/getMenuList",
+  async (name, { rejectWithValue }) => {
+    const response = await client.get(URL.MENULIST_API(name));
+    const json = await response.json();
+    if (!response.ok) {
+      return rejectWithValue(json);
     }
-  );
+    return json;
+  }
+);
 
-  export const getPreEnquiryData = createAsyncThunk(
-    "LOGIN_SLICE/getPreEnquiryData",
-    async (endUrl, { rejectWithValue }) => {
-      let url = URL.LEADS_LIST_API() + endUrl;
-      const response = await client.get(url);
-      const json = await response.json();
-      if (!response.ok) {
-        return rejectWithValue(json);
-      }
-      return json;
+export const getPreEnquiryData = createAsyncThunk(
+  "LOGIN_SLICE/getPreEnquiryData",
+  async (endUrl, { rejectWithValue }) => {
+    let url = URL.LEADS_LIST_API() + endUrl;
+    const response = await client.get(url);
+    const json = await response.json();
+    if (!response.ok) {
+      return rejectWithValue(json);
     }
-  );
+    return json;
+  }
+);
 
-  export const getCustomerTypeList = createAsyncThunk(
-    "LOGIN_SLICE/getCustomerTypeList",
-    async (data, { rejectWithValue }) => {
-      const response = await client.get(URL.CUSTOMER_TYPE());
-      const json = await response.json();
-      if (!response.ok) {
-        return rejectWithValue(json);
-      }
-      return json;
+export const getCustomerTypeList = createAsyncThunk(
+  "LOGIN_SLICE/getCustomerTypeList",
+  async (data, { rejectWithValue }) => {
+    const response = await client.get(URL.CUSTOMER_TYPE());
+    const json = await response.json();
+    if (!response.ok) {
+      return rejectWithValue(json);
     }
-  );
+    return json;
+  }
+);
 
-  export const getCarModalList = createAsyncThunk(
-    "LOGIN_SLICE/getCarModalList",
-    async (orgId, { rejectWithValue }) => {
-      const response = await client.get(URL.VEHICLE_MODELS(orgId));
-      const json = await response.json();
-      if (!response.ok) {
-        return rejectWithValue(json);
-      }
-      return json;
+export const getCarModalList = createAsyncThunk(
+  "LOGIN_SLICE/getCarModalList",
+  async (orgId, { rejectWithValue }) => {
+    const response = await client.get(URL.VEHICLE_MODELS(orgId));
+    const json = await response.json();
+    if (!response.ok) {
+      return rejectWithValue(json);
     }
-  );
+    return json;
+  }
+);
 
-  export const loginSlice = createSlice({
-    name: "LOGIN_SLICE",
-    initialState,
-    reducers: {
-      clearState: (state, action) => {
-        state.authToken = "";
-        state.userName = "";
-        state.status = "";
-        state.isLoading = false;
-        state.authToken = "";
-        state.userName = "";
-        state.showLoader = false;
-        state.offlineStatus = "";
-        state.userData = {};
-        state.empId = "";
-        state.menuListStatus = "";
-        state.branchesList = [];
-      },
-      updateEmployeeId: (state, action: PayloadAction<string>) => {
-        let employeeId = action.payload;
-        if (employeeId.length < 4) {
-          state.showLoginErr = false;
-          state.loginErrMessage = "";
-        }
-        state.employeeId = employeeId;
-      },
-      updatePassword: (state, action: PayloadAction<string>) => {
-        let password = action.payload;
-        if (password.length < 4) {
-          state.showPasswordErr = true;
-          state.passwordErrMessage = "Password max length is 6 chars";
-        } else {
-          state.showPasswordErr = false;
-          state.passwordErrMessage = "";
-        }
-        state.password = password;
-      },
-      updateSecurePassword: (state) => {
-        state.securePassword = !state.securePassword;
-      },
-      showErrorMessage: (state, action: PayloadAction<ErrorMessage>) => {
-        if (action.payload.key === "EMPLOYEEID") {
-          state.showLoginErr = true;
-          state.loginErrMessage = action.payload.message;
-        } else {
-          state.showPasswordErr = true;
-          state.passwordErrMessage = action.payload.message;
-        }
-      },
+export const loginSlice = createSlice({
+  name: "LOGIN_SLICE",
+  initialState,
+  reducers: {
+    clearState: (state, action) => {
+      state.authToken = "";
+      state.userName = "";
+      state.status = "";
+      state.isLoading = false;
+      state.authToken = "";
+      state.userName = "";
+      state.showLoader = false;
+      state.offlineStatus = "";
+      state.userData = {};
+      state.empId = "";
+      state.menuListStatus = "";
+      state.branchesList = [];
     },
-    extraReducers: (builder) => {
-      builder
-        .addCase(postUserData.pending, (state) => {
-          state.status = "loading";
-          state.isLoading = true;
-        })
-        .addCase(postUserData.fulfilled, (state, action) => {
-          console.log("res2: ", action.payload);
-          const dataObj = action.payload;
-          if (dataObj.status == "200") {
-            state.status = "sucess";
-            state.isLoading = false;
-            state.authToken = dataObj.idToken;
-            state.userName = dataObj.userName;
-            state.userData = dataObj;
-            AsyncStore.storeData(AsyncStore.Keys.USER_TOKEN, dataObj.idToken);
-          } else if (dataObj.reason) {
-            showToastRedAlert(dataObj.reason);
-          }
-        })
-        .addCase(postUserData.rejected, (state, action) => {
-          console.log("res3: ", action.payload);
-          state.status = "failed";
+    updateEmployeeId: (state, action: PayloadAction<string>) => {
+      let employeeId = action.payload;
+      if (employeeId.length < 4) {
+        state.showLoginErr = false;
+        state.loginErrMessage = "";
+      }
+      state.employeeId = employeeId;
+    },
+    updatePassword: (state, action: PayloadAction<string>) => {
+      let password = action.payload;
+      if (password.length < 4) {
+        state.showPasswordErr = true;
+        state.passwordErrMessage = "Password max length is 6 chars";
+      } else {
+        state.showPasswordErr = false;
+        state.passwordErrMessage = "";
+      }
+      state.password = password;
+    },
+    updateSecurePassword: (state) => {
+      state.securePassword = !state.securePassword;
+    },
+    showErrorMessage: (state, action: PayloadAction<ErrorMessage>) => {
+      if (action.payload.key === "EMPLOYEEID") {
+        state.showLoginErr = true;
+        state.loginErrMessage = action.payload.message;
+      } else {
+        state.showPasswordErr = true;
+        state.passwordErrMessage = action.payload.message;
+      }
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(postUserData.pending, (state) => {
+        state.status = "loading";
+        state.isLoading = true;
+      })
+      .addCase(postUserData.fulfilled, (state, action) => {
+        console.log("res2: ", action.payload);
+        const dataObj = action.payload;
+        if (dataObj.status == "200") {
+          state.status = "sucess";
           state.isLoading = false;
-          if (action.payload["errorMessage"]) {
-            showToastRedAlert(action.payload["errorMessage"]);
-          }
-        })
-        .addCase(getPreEnquiryData.pending, (state) => {
-          state.isLoading = true;
-          state.offlineStatus = "pending";
-        })
-        .addCase(getPreEnquiryData.fulfilled, (state, action) => {
-          console.log("res: ", action.payload);
-          const dmsEntityObj = action.payload?.dmsEntity;
-          if (dmsEntityObj) {
-            const data = dmsEntityObj.leadDtoPage.content;
-            if (data.length > 0) {
-              data.forEach((object) => {
-                realm.write(() => {
-                  realm.create("PRE_ENQUIRY_TABLE", { ...object });
-                });
-              });
-            }
-          }
-          state.isLoading = false;
-          state.offlineStatus = "completed";
-        })
-        .addCase(getPreEnquiryData.rejected, (state) => {
-          state.isLoading = false;
-          state.offlineStatus = "completed";
-        })
-        .addCase(getMenuList.pending, (state, action) => {
-          state.menuListStatus = "pending";
-          state.isLoading = true;
-        })
-        .addCase(getMenuList.fulfilled, (state, action) => {
-          const dmsEntityObj = action.payload.dmsEntity;
-          const empId = dmsEntityObj.loginEmployee.empId;
-
-          const menuList = dmsEntityObj.menuList;
-
-          if (menuList.length > 0) {
-            let newMenuList = [];
-            menuList.forEach((item) => {
-              newMenuList.push({
-                screen: item.menuId,
-                title: item.displayName,
+          state.authToken = dataObj.idToken;
+          state.userName = dataObj.userName;
+          state.userData = dataObj;
+          AsyncStore.storeData(AsyncStore.Keys.USER_TOKEN, dataObj.idToken);
+        } else if (dataObj.reason) {
+          showToastRedAlert(dataObj.reason);
+        }
+      })
+      .addCase(postUserData.rejected, (state, action) => {
+        console.log("res3: ", action.payload);
+        state.status = "failed";
+        state.isLoading = false;
+        if (action.payload["errorMessage"]) {
+          showToastRedAlert(action.payload["errorMessage"]);
+        }
+      })
+      .addCase(getPreEnquiryData.pending, (state) => {
+        state.isLoading = true;
+        state.offlineStatus = "pending";
+      })
+      .addCase(getPreEnquiryData.fulfilled, (state, action) => {
+        console.log("res: ", action.payload);
+        const dmsEntityObj = action.payload?.dmsEntity;
+        if (dmsEntityObj) {
+          const data = dmsEntityObj.leadDtoPage.content;
+          if (data.length > 0) {
+            data.forEach((object) => {
+              realm.write(() => {
+                realm.create("PRE_ENQUIRY_TABLE", { ...object });
               });
             });
-            state.menuList = newMenuList;
           }
+        }
+        state.isLoading = false;
+        state.offlineStatus = "completed";
+      })
+      .addCase(getPreEnquiryData.rejected, (state) => {
+        state.isLoading = false;
+        state.offlineStatus = "completed";
+      })
+      .addCase(getMenuList.pending, (state, action) => {
+        state.menuListStatus = "pending";
+        state.isLoading = true;
+      })
+      .addCase(getMenuList.fulfilled, (state, action) => {
+        const dmsEntityObj = action.payload.dmsEntity;
+        const empId = dmsEntityObj.loginEmployee.empId;
 
-          AsyncStore.storeData(AsyncStore.Keys.EMP_ID, empId.toString());
-          AsyncStore.storeData(
-            AsyncStore.Keys.LOGIN_EMPLOYEE,
-            JSON.stringify(dmsEntityObj.loginEmployee)
-          );
+        const menuList = dmsEntityObj.menuList;
 
-          state.login_employee_details = dmsEntityObj.loginEmployee;
-          state.branchesList = dmsEntityObj.loginEmployee.branchs;
-          state.empId = empId;
-          state.menuListStatus = "completed";
-          state.isLoading = false;
-        })
-        .addCase(getMenuList.rejected, (state, action) => {
-          state.menuListStatus = "failed";
-          state.isLoading = false;
-        })
-        .addCase(getCustomerTypeList.fulfilled, (state, action) => {
-          console.log("customer_type_list: ", action.payload);
-          const data = action.payload;
-          data.forEach((item) => {
-            realm.write(() => {
-              realm.create("CUSTOMER_TYPE_TABLE", {
-                id: item.id,
-                name: item.customerType,
-              });
+        if (menuList.length > 0) {
+          let newMenuList = [];
+          menuList.forEach((item) => {
+            newMenuList.push({
+              screen: item.menuId,
+              title: item.displayName,
             });
           });
-        })
-        .addCase(getCarModalList.fulfilled, (state, action) => {
-          console.log("vehicle_modal_list: ", action.payload);
-          const data = action.payload;
-          data.forEach((item) => {
-            realm.write(() => {
-              realm.create("CAR_MODAL_LIST_TABLE", {
-                id: item.vehicleId,
-                name: item.model,
-              });
+          state.menuList = newMenuList;
+        }
+
+        AsyncStore.storeData(AsyncStore.Keys.EMP_ID, empId.toString());
+        AsyncStore.storeData(
+          AsyncStore.Keys.LOGIN_EMPLOYEE,
+          JSON.stringify(dmsEntityObj.loginEmployee)
+        );
+
+        state.login_employee_details = dmsEntityObj.loginEmployee;
+        state.branchesList = dmsEntityObj.loginEmployee.branchs;
+        state.empId = empId;
+        state.menuListStatus = "completed";
+        state.isLoading = false;
+      })
+      .addCase(getMenuList.rejected, (state, action) => {
+        state.menuListStatus = "failed";
+        state.isLoading = false;
+      })
+      .addCase(getCustomerTypeList.fulfilled, (state, action) => {
+        console.log("customer_type_list: ", action.payload);
+        const data = action.payload;
+        data.forEach((item) => {
+          realm.write(() => {
+            realm.create("CUSTOMER_TYPE_TABLE", {
+              id: item.id,
+              name: item.customerType,
             });
           });
         });
-    },
-  });
+      })
+      .addCase(getCarModalList.fulfilled, (state, action) => {
+        console.log("vehicle_modal_list: ", action.payload);
+        const data = action.payload;
+        data.forEach((item) => {
+          realm.write(() => {
+            realm.create("CAR_MODAL_LIST_TABLE", {
+              id: item.vehicleId,
+              name: item.model,
+            });
+          });
+        });
+      });
+  },
+});
 
-  export const {
-    clearState,
-    updateEmployeeId,
-    updatePassword,
-    updateSecurePassword,
-    showErrorMessage,
-  } = loginSlice.actions;
+export const {
+  clearState,
+  updateEmployeeId,
+  updatePassword,
+  updateSecurePassword,
+  showErrorMessage,
+} = loginSlice.actions;
 
-  export default loginSlice.reducer;
+export default loginSlice.reducer;
 
 // export const postUserData = createAsyncThunk('LOGIN_SLICE/postUserData', async () => {
 //     const response = await client.get(ENDPOINT(KEYS.login))
