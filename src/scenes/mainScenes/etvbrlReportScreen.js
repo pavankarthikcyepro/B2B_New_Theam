f3b8364e01be625cbecbb1d848412ce0fae51b5b
mono-hyd-mac-5 <vikash.kumar@kelltontech.com>
2022-05-16 16:05:39 +0530

    Profile pic resolved

--- src/scenes/mainScenes/etvbrlReportScreen.js
+++ src/scenes/mainScenes/etvbrlReportScreen.js
@@ -112,9 +112,9 @@ const etvbrlReportScreen = () => {
             toDate: selectedToDate
         }
         console.log("downloadReport payload", payload)
+        console.log("download Url", URL.DOWNLOAD_REPORT())
         const response = await client.post(URL.DOWNLOAD_REPORT(), payload);
         const report = await response.json();
-        console.log("download Url", report)
         const { config, fs } = RNFetchBlob;
         let downloadDir = fs.dirs.DownloadDir;
         let date = new Date();
