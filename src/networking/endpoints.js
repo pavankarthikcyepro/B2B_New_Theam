f6c81b51cef804e4f3d0be50b90b7fb3dc5006f7
Sagar patel <sagar@ninjatechnolabs.com>
2022-11-10 18:44:09 +0530

    Intergrated an API for the ETVBRL report

--- src/networking/endpoints.js
+++ src/networking/endpoints.js
@@ -42,6 +42,7 @@ export const lostSubLost = "http://automatestaging-724985329.ap-south-1.elb.amaz
 export const getBranch = "http://automatestaging-724985329.ap-south-1.elb.amazonaws.com:8092/oh";
 export const downloadFile = "http://automatestaging-724985329.ap-south-1.elb.amazonaws.com:8093/dynamic-reports";
 export const downloadFile1 = "http://automatestaging-724985329.ap-south-1.elb.amazonaws.com:8095/dynamic-reports/etvbrl_report_download";
+export const downloadFile2 = "http://automatestaging-724985329.ap-south-1.elb.amazonaws.com:8095/dynamic-reports/etvbrl_report";
 export const tasktransfer = "http://automatestaging-724985329.ap-south-1.elb.amazonaws.com:8092/sales-gap/target-dropdown";
 export const getTaskList = "http://automatestaging-724985329.ap-south-1.elb.amazonaws.com:8081/sales/task-delegation/get_task_list";
 export const getEmployeeData = "http://automatestaging-724985329.ap-south-1.elb.amazonaws.com:8089/role-management/employee/dept-employees";
@@ -356,7 +357,7 @@ const URL = {
     return getBranch + `/data-nodes?orgId=1&levelCode=Level5`;
   },
   DOWNLOAD_FILE: () => {
-    return downloadFile1;
+    return downloadFile2;
   },
   LOCATION_LIST: (orgId) => {
     return `http://automatestaging-724985329.ap-south-1.elb.amazonaws.com:8092/oh/data-nodes?orgId=${orgId}&levelCode=Level4`;
