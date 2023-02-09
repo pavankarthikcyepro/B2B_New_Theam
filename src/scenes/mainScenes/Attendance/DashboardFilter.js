3724a57003062d8f4dc3261e407bf27cf49bca07
Sagar patel <sagar@ninjatechnolabs.com>
2023-02-09 14:59:19 +0530

    local

--- src/scenes/mainScenes/Attendance/DashboardFilter.js
+++ src/scenes/mainScenes/Attendance/DashboardFilter.js
@@ -120,6 +120,7 @@ const FilterAttendanceDashBoardScreen = ({ route, navigation }) => {
       for (let key in selector.filter_drop_down_data) {
         names.push(key);
       }
+      console.log(names);
       setNameKeyList(names);
       setTotalDataObj(selector.filter_drop_down_data);
     }
