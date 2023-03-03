80a5ab4f776b093d0d3f9935d395bb7f061a2ed1
Vishal <vin@ninjatechnolabs.com>
2023-03-03 14:56:32 +0530

    Added new notification ui

--- src/pureComponents/notificationItem.js
+++ src/pureComponents/notificationItem.js
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet, View, Text, TouchableOpacity, Image } from "react-native";
 import { IconButton } from "react-native-paper";
 import { Colors, GlobalStyle } from "../styles";
+import ReadMore from "react-native-read-more-text";
 
 export const NotificationItem = ({ title, date, onPress, icon, style, isFlag }) => {
   const conversionIndex = title.includes("Conversion :")
@@ -19,26 +20,45 @@ export const NotificationItem = ({ title, date, onPress, icon, style, isFlag }) 
     thirdStr = title.slice(perIndex + 1, title.length);
   }
 
+  let isConsAvailable = title.includes("Consultants :");
+  if (!isConsAvailable) {
+    isConsAvailable = title.includes("Consultants:");
+  }
+
+  const consStartIndex = title.indexOf("[");
+  const consEndIndex = title.indexOf("]");
+
+  if (consStartIndex >= 0 && consEndIndex >= 0) {
+    firstStr = title.slice(0, consStartIndex) + "\n";
+    secStr = title.slice(consStartIndex + 1, consEndIndex);
+    thirdStr = title.slice(consEndIndex + 1, title.length);
+    secStr = secStr.split(", ").join(",\n");
+  }
+
   return (
     <TouchableOpacity style={[styles.itemContainer, style]} onPress={onPress}>
       <View style={styles.subRow}>
         <Image source={icon} style={styles.iconContainer} />
         <View style={{ margin: 5, flex: 1 }}>
           {secStr ? (
-            <Text style={styles.title}>
+            <ReadMore numberOfLines={4} style={styles.title}>
               {firstStr}
               <Text
                 style={[
                   styles.perText,
-                  isFlag == "Y" ? { color: Colors.RED, fontWeight: "bold" } : null,
+                  isFlag == "Y"
+                    ? { color: Colors.RED, fontWeight: "bold" }
+                    : null,
                 ]}
               >
                 {secStr}
               </Text>
               <Text style={styles.title}>{thirdStr}</Text>
-            </Text>
+            </ReadMore>
           ) : (
-            <Text style={styles.title}>{title}</Text>
+            <ReadMore numberOfLines={4} style={styles.title}>
+              {title}
+            </ReadMore>
           )}
         </View>
       </View>
