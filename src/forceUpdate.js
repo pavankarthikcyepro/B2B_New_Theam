97eb24bbde1e8c3afc86a6c9623b7bfcc286e7df
Sagar patel <sagar@ninjatechnolabs.com>
2023-03-24 12:04:35 +0530

    local

--- src/forceUpdate.js
+++ src/forceUpdate.js
@@ -1,3 +1,3 @@
-export const VersionName = "1.7";
-export const VersionCode = "1.7.6";
+export const VersionName = "1.8";
+export const VersionCode = "1.0";
 export const VersionString = VersionName + "(" + VersionCode + ")";
