4fffb956bc2bfcd0c9fa377a6a039674dccfd84b
Vishal <vishal@cyepro.com>
2023-03-28 18:19:45 +0530

    Fix > task 360 screen showing previous data

--- src/redux/taskThreeSixtyReducer.ts
+++ src/redux/taskThreeSixtyReducer.ts
@@ -73,134 +73,133 @@ export const getTestDriveHistoryCount = createAsyncThunk("TASK_360_SLICE/getTest
 
 
 const taskThreeSixtySlice = createSlice({
-    name: "TASK_360_SLICE",
-    initialState: {
-        status: "",
-        isLoading: false,
-        wrokflow_response: {},
-        wrokflow_response_status: "",
-        enquiry_leadDto_response: {},
-        enquiry_leadDto_response_status: "",
-        leadAge: 0,
-        taskThreeSixtyHistory: [],
-        followUpCount :[],
-        followUpcount_Status : "",
-        testDrivCount:0,
+  name: "TASK_360_SLICE",
+  initialState: {
+    status: "",
+    isLoading: false,
+    wrokflow_response: [],
+    wrokflow_response_status: "",
+    enquiry_leadDto_response: {},
+    enquiry_leadDto_response_status: "",
+    leadAge: 0,
+    taskThreeSixtyHistory: [],
+    followUpCount: [],
+    followUpcount_Status: "",
+    testDrivCount: 0,
+  },
+  reducers: {
+    clearState: (state, action) => {
+      state.leadAge = 0;
+      state.taskThreeSixtyHistory = [];
+      state.followUpcount_Status = "";
+      state.testDrivCount = 0;
     },
-    reducers: {
-        clearState: (state, action) => {
-            state.leadAge = 0;
-            state.taskThreeSixtyHistory = [];
-            state.followUpcount_Status = "",
-            state.testDrivCount = 0
-        },
+    clearListData: (state, action) => {
+      state.enquiry_leadDto_response = {};
+      state.enquiry_leadDto_response_status = "";
+      state.wrokflow_response = [];
+      state.wrokflow_response_status = "pending";
+      state.taskThreeSixtyHistory = [];
     },
-    extraReducers: (builder) => {
-        // Get Workflow Details Api
-        builder.addCase(getWorkFlow.pending, (state, action) => {
-            state.wrokflow_response = [];
-            state.isLoading = true;
-            state.wrokflow_response_status = "pending";
-        })
-        builder.addCase(getWorkFlow.fulfilled, (state, action) => {
-            if (action.payload?.dmsEntity) {
-                if (action.payload.dmsEntity?.tasks) {
-                    state.wrokflow_response = action.payload.dmsEntity.tasks;
-                }
-            }
-            state.wrokflow_response_status = "success";
-            state.isLoading = false;
-        })
-        builder.addCase(getWorkFlow.rejected, (state, action) => {
-            state.wrokflow_response = [];
-            state.wrokflow_response_status = "failed";
-            state.isLoading = false;
-        })
-        // Get Enquiry Details Api
-        builder.addCase(getEnquiryDetails.pending, (state, action) => {
-            state.enquiry_leadDto_response = {};
-            state.enquiry_leadDto_response_status = "pending";
-            state.isLoading = true;
-        })
-        builder.addCase(getEnquiryDetails.fulfilled, (state, action) => {
-            if (action.payload?.dmsEntity) {
-                if (action.payload.dmsEntity?.dmsLeadDto) {
-                    state.enquiry_leadDto_response = action.payload.dmsEntity.dmsLeadDto;
-                }
-            }
-            state.enquiry_leadDto_response_status = "success";
-            state.isLoading = false;
-        })
-        builder.addCase(getEnquiryDetails.rejected, (state, action) => {
-            state.enquiry_leadDto_response = {};
-            state.enquiry_leadDto_response_status = "failed";
-            state.isLoading = false;
-        })
+  },
+  extraReducers: (builder) => {
+    // Get Workflow Details Api
+    builder.addCase(getWorkFlow.pending, (state, action) => {
+      state.wrokflow_response_status = "pending";
+      state.isLoading = true;
+    });
+    builder.addCase(getWorkFlow.fulfilled, (state, action) => {
+      if (action.payload?.dmsEntity) {
+        if (action.payload.dmsEntity?.tasks) {
+          state.wrokflow_response = action.payload.dmsEntity.tasks;
+        }
+      }
+      state.wrokflow_response_status = "success";
+      state.isLoading = false;
+    });
+    builder.addCase(getWorkFlow.rejected, (state, action) => {
+      state.wrokflow_response_status = "failed";
+      state.isLoading = false;
+    });
+    // Get Enquiry Details Api
+    builder.addCase(getEnquiryDetails.pending, (state, action) => {
+      state.enquiry_leadDto_response_status = "pending";
+      state.isLoading = true;
+    });
+    builder.addCase(getEnquiryDetails.fulfilled, (state, action) => {
+      if (action.payload?.dmsEntity) {
+        if (action.payload.dmsEntity?.dmsLeadDto) {
+          state.enquiry_leadDto_response = action.payload.dmsEntity.dmsLeadDto;
+        }
+      }
+      state.enquiry_leadDto_response_status = "success";
+    //   state.isLoading = false;
+    });
+    builder.addCase(getEnquiryDetails.rejected, (state, action) => {
+      state.enquiry_leadDto_response_status = "failed";
+      state.isLoading = false;
+    });
 
-        builder.addCase(getLeadAge.pending, (state, action) => {
-            state.leadAge = 0
-        })
-        builder.addCase(getLeadAge.fulfilled, (state, action) => {
-            if (action.payload?.leadAge) {
-                state.leadAge = action.payload?.leadAge
-            }
-        })
-        builder.addCase(getLeadAge.rejected, (state, action) => {
-            state.leadAge = 0
-        })
-        
-        builder.addCase(getTaskThreeSixtyHistory.pending, (state, action) => {
-            state.taskThreeSixtyHistory = [];
-            state.isLoading = true;
-        })
-        builder.addCase(getTaskThreeSixtyHistory.fulfilled, (state, action) => {
-            if (action?.payload?.dmsEntity?.taskss) {
-              state.taskThreeSixtyHistory = action.payload.dmsEntity.taskss;
-            }
-            state.isLoading = false;
-        })
-        builder.addCase(getTaskThreeSixtyHistory.rejected, (state, action) => {
-            state.taskThreeSixtyHistory = [];
-            state.isLoading = false;
-        })
+    builder.addCase(getLeadAge.pending, (state, action) => {
+      state.leadAge = 0;
+    });
+    builder.addCase(getLeadAge.fulfilled, (state, action) => {
+      if (action.payload?.leadAge) {
+        state.leadAge = action.payload?.leadAge;
+      }
+    });
+    builder.addCase(getLeadAge.rejected, (state, action) => {
+      state.leadAge = 0;
+    });
 
-        builder.addCase(getFollowUPCount.pending, (state, action) => {
-            state.followUpCount = [];
-            state.isLoading = true;
-            state.followUpcount_Status = "pending"
-        })
-        builder.addCase(getFollowUPCount.fulfilled, (state, action) => {
-            
-                state.followUpCount = action.payload;
-            state.followUpcount_Status = "fulfilled"
-            state.isLoading = false;
-        })
-        builder.addCase(getFollowUPCount.rejected, (state, action) => {
-            state.followUpCount = [];
-            state.isLoading = false;
-            state.followUpcount_Status = "rejected"
-        })
+    builder.addCase(getTaskThreeSixtyHistory.pending, (state, action) => {
+      state.taskThreeSixtyHistory = [];
+      state.isLoading = true;
+    });
+    builder.addCase(getTaskThreeSixtyHistory.fulfilled, (state, action) => {
+      if (action?.payload?.dmsEntity?.taskss) {
+        state.taskThreeSixtyHistory = action.payload.dmsEntity.taskss;
+      }
+      state.isLoading = false;
+    });
+    builder.addCase(getTaskThreeSixtyHistory.rejected, (state, action) => {
+      state.taskThreeSixtyHistory = [];
+      state.isLoading = false;
+    });
 
+    builder.addCase(getFollowUPCount.pending, (state, action) => {
+      state.followUpCount = [];
+      state.isLoading = true;
+      state.followUpcount_Status = "pending";
+    });
+    builder.addCase(getFollowUPCount.fulfilled, (state, action) => {
+      state.followUpCount = action.payload;
+      state.followUpcount_Status = "fulfilled";
+      state.isLoading = false;
+    });
+    builder.addCase(getFollowUPCount.rejected, (state, action) => {
+      state.followUpCount = [];
+      state.isLoading = false;
+      state.followUpcount_Status = "rejected";
+    });
 
-
-        builder.addCase(getTestDriveHistoryCount.pending, (state, action) => {
-            state.testDrivCount = 0;
-            state.isLoading = true;
-            // state.followUpcount_Status = "pending"
-        })
-        builder.addCase(getTestDriveHistoryCount.fulfilled, (state, action) => {
-
-            state.testDrivCount = action.payload.count;
-            // state.followUpcount_Status = "fulfilled"
-            state.isLoading = false;
-        })
-        builder.addCase(getTestDriveHistoryCount.rejected, (state, action) => {
-            state.testDrivCount = 0;
-            state.isLoading = false;
-            // state.followUpcount_Status = "rejected"
-        })
-    }
+    builder.addCase(getTestDriveHistoryCount.pending, (state, action) => {
+      state.testDrivCount = 0;
+      state.isLoading = true;
+      // state.followUpcount_Status = "pending"
+    });
+    builder.addCase(getTestDriveHistoryCount.fulfilled, (state, action) => {
+      state.testDrivCount = action.payload.count;
+      // state.followUpcount_Status = "fulfilled"
+      state.isLoading = false;
+    });
+    builder.addCase(getTestDriveHistoryCount.rejected, (state, action) => {
+      state.testDrivCount = 0;
+      state.isLoading = false;
+      // state.followUpcount_Status = "rejected"
+    });
+  },
 });
 
-export const { clearState } = taskThreeSixtySlice.actions;
+export const { clearState, clearListData } = taskThreeSixtySlice.actions;
 export default taskThreeSixtySlice.reducer;
