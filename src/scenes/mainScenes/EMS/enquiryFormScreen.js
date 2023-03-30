80037cefbb68f0b2fd1c8fef96e1e2d345f91b89
Sagar patel <sagar@ninjatechnolabs.com>
2023-03-30 15:05:44 +0530

    duplicate issue

--- src/scenes/mainScenes/EMS/enquiryFormScreen.js
+++ src/scenes/mainScenes/EMS/enquiryFormScreen.js
@@ -1135,6 +1135,7 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
         scrollToPos(0);
         setOpenAccordian("2");
         showToast("Please select Gender");
+        setIsSubmitPress(false);
         return;
       }
     }
@@ -1143,6 +1144,7 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
       scrollToPos(0);
       setOpenAccordian("2");
       showToast("please enter alphabetics only in firstname");
+      setIsSubmitPress(false);
       return;
     }
 
@@ -1150,6 +1152,7 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
       scrollToPos(0);
       setOpenAccordian("2");
       showToast("please enter alphabetics only in lastname");
+      setIsSubmitPress(false);
       return;
     }
 
@@ -1157,6 +1160,7 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
       scrollToPos(2);
       setOpenAccordian("1");
       showToast("Please select enquiry segment");
+      setIsSubmitPress(false);
       return;
     }
 
@@ -1164,6 +1168,7 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
       scrollToPos(2);
       setOpenAccordian("1");
       showToast("Please select customer type");
+      setIsSubmitPress(false);
       return;
     }
 
@@ -1171,6 +1176,7 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
       scrollToPos(2);
       setOpenAccordian("1");
       showToast("Please fill  Buyer type");
+      setIsSubmitPress(false);
       return;
     }
 
@@ -1195,6 +1201,7 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
       scrollToPos(14);
       setOpenAccordian("3");
       showToast("Please fill Permanent pincode");
+      setIsSubmitPress(false);
       return;
     }
 
@@ -1202,12 +1209,16 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
       scrollToPos(14);
       setOpenAccordian("3");
       showToast("Please fill Permanent Urban or Rural");
+      setIsSubmitPress(false);
+
       return;
     }
 
     if (checkModelSelection()) {
       scrollToPos(4);
       setOpenAccordian("4");
+      setIsSubmitPress(false);
+
       return;
     }
 
@@ -1220,6 +1231,8 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
       scrollToPos(4);
       setOpenAccordian("4");
       showToast("Select is Primary for atleast one vehicle");
+      setIsSubmitPress(false);
+
       return;
     }
 
@@ -1228,6 +1241,8 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
       scrollToPos(5);
       setOpenAccordian("5");
       showToast("Please fill required fields in Finance Details");
+      setIsSubmitPress(false);
+
       return;
     }
     // if (selector.retail_finance === "In House") {
@@ -1293,6 +1308,8 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
         scrollToPos(8);
         setOpenAccordian("8");
         showToast("Please enter alphabetics only in color ");
+        setIsSubmitPress(false);
+
         return;
       }
     }
@@ -1306,6 +1323,8 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
           scrollToPos(9);
           setOpenAccordian("9");
           showToast("Please enter alphabetics only in color ");
+          setIsSubmitPress(false);
+
           return;
         }
       }
@@ -1313,12 +1332,15 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
         scrollToPos(9);
         setOpenAccordian("9");
         showToast("Please fill reg no is mandatory");
+        setIsSubmitPress(false);
+
         return;
       }
       if (!isValidateAlphabetics(selector.r_model_other_name)) {
         scrollToPos(9);
         setOpenAccordian("9");
         showToast("Please enter proper model other name");
+        setIsSubmitPress(false);
         return;
       }
 
@@ -1333,6 +1355,7 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
             scrollToPos(9);
             setOpenAccordian("9");
             showToast("Please enter the proper Hypothication name");
+            setIsSubmitPress(false);
             return;
           }
         }
@@ -1341,6 +1364,7 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
             scrollToPos(9);
             setOpenAccordian("9");
             showToast("Please enter the proper Hypothication branch");
+            setIsSubmitPress(false);
             return;
           }
         }
@@ -1353,6 +1377,7 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
           scrollToPos(7);
           setOpenAccordian("7");
           showToast("please enter the validate Dealership name");
+          setIsSubmitPress(false);
           return;
         }
       }
@@ -1370,6 +1395,7 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
       scrollToPos(6);
       setOpenAccordian("6");
       showToast("Please enter proper PAN number");
+      setIsSubmitPress(false);
       return;
     }
 
@@ -1377,6 +1403,7 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
       scrollToPos(6);
       setOpenAccordian("6");
       showToast("Please enter proper Aadhaar number");
+      setIsSubmitPress(false);
       return;
     }
 
@@ -1387,10 +1414,12 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
       scrollToPos(6);
       setOpenAccordian("6");
       showToast("Please enter proper gstin number");
+      setIsSubmitPress(false);
       return;
     }
 
     if (!selector.enquiry_details_response) {
+      setIsSubmitPress(false);
       return;
     }
 
@@ -2293,46 +2322,48 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
     }
   }, [selector.update_enquiry_details_response]);
 
-
   useEffect(() => {
-    if (leadStatus === "ENQUIRYCOMPLETED" &&
+    if (
+      leadStatus === "ENQUIRYCOMPLETED" &&
       leadStage === "ENQUIRY" &&
       carModelsList &&
-      carModelsList.length >0){
-      navigation.setOptions(
-        {
-         
-          headerRight: () => (
-            <TouchableOpacity
+      carModelsList.length > 0
+    ) {
+      navigation.setOptions({
+        headerRight: () => (
+          <TouchableOpacity
+            style={{
+              width: 110,
+              height: 30,
+              borderRadius: 15,
+              borderColor: Colors.RED,
+              borderWidth: 1,
+              justifyContent: "center",
+              alignItems: "center",
+              marginRight: 5,
+              flexDirection: "row",
+            }}
+            onPress={() => {
+              navigateToProforma();
+            }}
+          >
+            <Text
               style={{
-                width: 110,
-                height: 30,
-                borderRadius: 15,
-                borderColor: Colors.RED,
-                borderWidth: 1,
-                justifyContent: "center",
-                alignItems: "center",
-                marginRight: 5,
-                flexDirection: "row",
+                fontSize: 12,
+                fontWeight: "600",
+                color: "#fff",
+                marginLeft: 5,
               }}
-              onPress={()=>{navigateToProforma()}}
             >
-             
-              <Text
-                style={{
-                  fontSize: 12,
-                  fontWeight: "600",
-                  color: "#fff",
-                  marginLeft: 5,
-                }}
-              >Proforma Invoice</Text>
-              {/* <Text style={{ fontSize: 15, fontWeight: '600', color: '#fff' }}>200 days</Text> */}
-            </TouchableOpacity>
-          ),
-        });
-      }
-  }, [leadStatus, carModelsList])
-  
+              Proforma Invoice
+            </Text>
+            {/* <Text style={{ fontSize: 15, fontWeight: '600', color: '#fff' }}>200 days</Text> */}
+          </TouchableOpacity>
+        ),
+      });
+    }
+  }, [leadStatus, carModelsList]);
+
   const getEnquiryListFromServer = () => {
     if (userData.employeeId) {
       let endUrl =
@@ -3264,7 +3295,6 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
           ref={scrollRef}
         >
           <View style={styles.baseVw}>
-
             {/* {leadStatus === "ENQUIRYCOMPLETED" &&
               leadStage === "ENQUIRY" &&
               carModelsList &&
@@ -6218,6 +6248,7 @@ const DetailsOverviewScreen = ({ route, navigation }) => {
                 style={{ width: 120 }}
                 color={Colors.PINK}
                 labelStyle={{ textTransform: "none" }}
+                disabled={isSubmitPress}
                 onPress={submitClicked}
               >
                 Submit
@@ -6534,7 +6565,7 @@ const styles = StyleSheet.create({
     width: "100%",
     alignItems: "flex-end",
     marginVertical: 6,
-    marginBottom:10
+    marginBottom: 10,
   },
   tochable1: {
     width: 140,
