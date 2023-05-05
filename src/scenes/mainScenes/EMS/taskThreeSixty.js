bde59382623d9470dfff63254d0f5a57bc6d2b24
Vishal <vishal@cyepro.com>
2023-05-05 11:39:15 +0530

    Added hour ui in task 360

--- src/scenes/mainScenes/EMS/taskThreeSixty.js
+++ src/scenes/mainScenes/EMS/taskThreeSixty.js
@@ -276,6 +276,24 @@ const TaskThreeSixtyScreen = ({ route, navigation }) => {
     });
   };
 
+  const checkForEnqFollow = (item, section) => {
+    if (
+      item.taskName.includes("Enquiry Follow Up") &&
+      section?.title == "Planned Tasks"
+    ) {
+      let formate = "DD/MM/YYYY hh:mm a";
+      const { taskUpdatedTime } = item;
+      const current = moment().format(formate);
+      const startDate = moment(current, formate);
+      const create = moment(taskUpdatedTime).format(formate);
+      const createDate = moment(create, formate);
+      const timeDiff = startDate.diff(createDate, "hours");
+      return timeDiff;
+    } else {
+      return 0;
+    }
+  };
+
   const renderItem = ({ item, index, section }) => {
     let isHistory = section.title == "Planned Tasks";
     let isDotVisible =
@@ -324,7 +342,7 @@ const TaskThreeSixtyScreen = ({ route, navigation }) => {
       }
 
       return true;
-    };
+    }
 
     const date = moment(item.taskUpdatedTime)
       .format("DD/MM/YY h:mm a")
@@ -333,6 +351,8 @@ const TaskThreeSixtyScreen = ({ route, navigation }) => {
     let radioColor =
       item.taskStatus === "CLOSED" ? Colors.DARK_GREEN : Colors.LIGHT_GRAY2;
 
+    let isHoursVisible = checkForEnqFollow(item, section) >= 2;
+
     return (
       <View style={styles.itemContainer}>
         <View style={styles.radioContainer}>
@@ -340,11 +360,26 @@ const TaskThreeSixtyScreen = ({ route, navigation }) => {
         </View>
         <TouchableOpacity
           onPress={() => itemClicked(item)}
-          style={styles.itemDetailsContainer}
+          style={[
+            styles.itemDetailsContainer,
+            isHoursVisible ? styles.cardBorder : null,
+          ]}
         >
-          <Text style={styles.dateTimeText}>
-            {date[0]} | {date[1] + " " + date[2]}
-          </Text>
+          {isHoursVisible ? (
+            <View style={styles.itemTopRow}>
+              <View style={styles.hourContainer}>
+                <Text style={styles.hourText}>{`>${checkForEnqFollow(item, section)}hrs`}</Text>
+              </View>
+              <Text style={styles.dateTimeText}>
+                {date[0]} | {date[1] + " " + date[2]}
+              </Text>
+            </View>
+          ) : (
+            <Text style={styles.dateTimeText}>
+              {date[0]} | {date[1] + " " + date[2]}
+            </Text>
+          )}
+
           {TaskNameView(item.taskName)}
           <Text style={styles.assigneeNameText}>
             {"Assignee: " + item.assignee?.empName}
@@ -577,6 +612,28 @@ const styles = StyleSheet.create({
     padding: 10,
     backgroundColor: Colors.WHITE,
     marginHorizontal: 10,
+    borderRadius: 5,
+  },
+  cardBorder: {
+    borderLeftColor: Colors.PINK,
+    borderLeftWidth: 1.5,
+  },
+  itemTopRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 3
+  },
+  hourContainer: {
+    borderRadius: 10,
+    backgroundColor: Colors.PINK,
+    paddingVertical: 2,
+    paddingHorizontal: 5,
+  },
+  hourText: {
+    fontSize: 11,
+    fontWeight: "500",
+    color: Colors.WHITE,
   },
   dateTimeText: {
     fontSize: 12,
@@ -689,6 +746,6 @@ const styles = StyleSheet.create({
     height: "80%",
     width: 1,
     backgroundColor: Colors.LIGHT_GRAY2,
-    alignSelf: "center"
+    alignSelf: "center",
   },
 });
